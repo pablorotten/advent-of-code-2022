816c450a2c5c99eadd6b4dd573851aff0bc2ee39
PRO <pro@n-side.com>
2022-12-03 19:12:13 +0100

    day 1

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -1,4 +1,24 @@
 package day1
 
-class Day1 {
+object Day1 {
+  @JvmStatic
+  fun main(args: Array<String>) {
+    val fileContent = this::class.java.getResourceAsStream("/day1/input.txt")
+
+    fileContent.bufferedReader().useLines { lines ->
+      var max = 0
+      var current = 0
+      lines.forEach { line ->
+        if(line.isNotEmpty())
+          current = current + line.toInt()
+        else {
+          if(current > max) max = current
+          current = 0
+        }
+
+      }
+
+      println("max $max")
+    }
+  }
 }
