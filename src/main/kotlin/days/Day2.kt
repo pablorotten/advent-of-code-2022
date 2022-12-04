9f9583dbddd082bacf902db269cad03c0fb13bb6
PRO <pro@n-side.com>
2022-12-04 13:12:49 +0100

    day 2 part two

--- src/main/kotlin/days/Day2.kt
+++ src/main/kotlin/days/Day2.kt
@@ -2,24 +2,53 @@ package days
 
 import util.readInput
 
-val points = mapOf("A" to 1, "B" to 2, "C" to 3, "X" to 1, "Y" to 2, "Z" to 3)
 fun main() {
   val input = readInput("day2")
-  var totalPoints = 0
+  fun part1(): Int {
+    val points = mapOf("A" to 1, "B" to 2, "C" to 3, "X" to 1, "Y" to 2, "Z" to 3)
+    var totalPoints = 0
 
-  input.map { line ->
-    val player1 = line.getOrElse(0) { ' ' }.toString()
-    val player2 = line.getOrElse(2) { ' ' }.toString()
+    input.map { line ->
+      val player1 = line.getOrElse(0) { ' ' }.toString()
+      val player2 = line.getOrElse(2) { ' ' }.toString()
 
-    if (arrayOf("A", "B", "C").contains(player1) && arrayOf("X", "Y", "Z").contains(player2)) {
+      if (arrayOf("A", "B", "C").contains(player1) && arrayOf("X", "Y", "Z").contains(player2)) {
 
-      val gamePoints = when (player1 to player2) {
-        "A" to "Y", "B" to "Z", "C" to "X" -> 6
-        else -> if (points[player1] == points[player2]) 3 else 0
+        val gamePoints = when (player1 to player2) {
+          "A" to "Y", "B" to "Z", "C" to "X" -> 6
+          else -> if (points[player1] == points[player2]) 3 else 0
+        }
+
+        totalPoints += gamePoints + points[player2]!!
       }
-
-      totalPoints += gamePoints + points[player2]!!
     }
+    return totalPoints
   }
-  println("totalPoints:$totalPoints")
+
+  // X lose
+  // Y draw
+  // Z win
+  fun part2(): Int {
+    val points = mapOf(
+      "A" to mapOf("X" to 3, "Y" to 1, "Z" to 2),
+      "B" to mapOf("X" to 1, "Y" to 2, "Z" to 3),
+      "C" to mapOf("X" to 2, "Y" to 3, "Z" to 1)
+    )
+    val results = mapOf("X" to 0, "Y" to 3, "Z" to 6)
+    var totalPoints = 0
+
+    input.map { line ->
+      val player1 = line.getOrElse(0) { ' ' }.toString()
+      val result = line.getOrElse(2) { ' ' }.toString()
+
+
+      val gamePoints = results[result]!! + points[player1]!![result]!!
+
+      totalPoints += gamePoints
+    }
+    return totalPoints
+  }
+
+  println(part1())
+  println(part2())
 }
