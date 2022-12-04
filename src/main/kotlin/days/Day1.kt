44ca58848c7acef809fa1e395486d8a2969c76c5
PRO <pro@n-side.com>
2022-12-04 12:54:29 +0100

    day 1 part two

--- src/main/kotlin/days/Day1.kt
+++ src/main/kotlin/days/Day1.kt
@@ -3,19 +3,40 @@ package days
 import util.readInput
 
 fun main() {
+
   val input = readInput("day1")
-  var max = 0
-  var current = 0
 
-  input.map { line ->
-    if (line.isNotEmpty())
-      current += line.toInt()
-    else {
-      if (current > max) max = current
-      current = 0
+  fun part1(): Int {
+    var max = 0
+    var current = 0
+
+    input.map { line ->
+      if (line.isNotEmpty())
+        current += line.toInt()
+      else {
+        if (current > max) max = current
+        current = 0
+      }
     }
-
+    return max
   }
 
-  println("max $max")
+  fun part2(): Int {
+    var max = 0
+    var calories = 0
+    val elvesCalories = mutableListOf<Int>()
+
+    input.map { line ->
+      if (line.isNotEmpty())
+        calories += line.toInt()
+      else {
+        elvesCalories.add(calories)
+        calories = 0
+      }
+    }
+    return elvesCalories.sorted().takeLast(3).sum()
+  }
+
+  println(part1())
+  println(part2())
 }
