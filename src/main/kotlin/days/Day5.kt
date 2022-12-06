e3e109b5472b7b776e51ee3e988edafd8793b8ec
PRO <pro@n-side.com>
2022-12-06 23:51:25 +0100

    day 5 part 2

--- src/main/kotlin/days/Day5.kt
+++ src/main/kotlin/days/Day5.kt
@@ -7,7 +7,8 @@ fun main() {
 
   val input = readInput("day5")
 
-  fun part1(): String {
+  fun initialStacks():Map<Int, LinkedList<Char>> {
+
     val stacks = mapOf(
       1 to LinkedList<Char>(),
       2 to LinkedList<Char>(),
@@ -29,6 +30,13 @@ fun main() {
         }
       }
 
+    return stacks
+  }
+
+  fun part1(): String {
+
+    val stacks = initialStacks()
+
     input.drop(10).map {
       it.replace("[^-?0-9]+".toRegex(), " ").trim().split(" ").let { l ->
         for (i in 1..l[0].toInt()) stacks[l[2].toInt()]!!.addFirst(stacks[l[1].toInt()]!!.remove())
@@ -39,5 +47,23 @@ fun main() {
   }
 
 
+  fun part2(): String {
+
+    val stacks = initialStacks()
+
+    input.drop(10).map {
+      it.replace("[^-?0-9]+".toRegex(), " ").trim().split(" ").let { l ->
+        val crane = stacks[l[1].toInt()]!!.take(l[0].toInt())
+        crane.forEach{stacks[l[1].toInt()]!!.removeFirst()}
+        crane.reversed().map { crate ->
+          stacks[l[2].toInt()]!!.addFirst(crate)
+        }
+      }
+    }
+
+    return stacks.values.fold("") { top, stack -> top + stack[0] }
+  }
+
   println(part1())
+  println(part2())
 }
