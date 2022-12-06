2d4a41d8249a616d7e7dc8da2a4f264b80ab5274
PRO <pro@n-side.com>
2022-12-06 22:53:45 +0100

    day 5 part 1

---
+++ src/main/kotlin/days/Day5.kt
@@ -0,0 +1,46 @@
+package days
+
+import util.readInput
+import java.util.*
+import kotlin.math.min
+
+fun main() {
+
+  val input = readInput("day5")
+
+  fun part1(): String {
+    val stacks = mapOf(
+      1 to LinkedList<Char>(),
+      2 to LinkedList<Char>(),
+      3 to LinkedList<Char>(),
+      4 to LinkedList<Char>(),
+      5 to LinkedList<Char>(),
+      6 to LinkedList<Char>(),
+      7 to LinkedList<Char>(),
+      8 to LinkedList<Char>(),
+      9 to LinkedList<Char>()
+    )
+
+    val stacksLines = input.take(8)
+
+    for(line in stacksLines)
+      line.chunked(4).forEachIndexed { index, chunk ->
+        chunk.getOrNull(1)?.let { crate ->
+          if(crate.isLetter()) stacks[index+1]?.add(crate)
+        }
+    }
+
+    input.drop(10).map {line ->
+      line.replace("[^-?0-9]+".toRegex(), " ").trim().split(" ")?.let { l ->
+        for (i in 1..l[0].toInt()) {
+          stacks[l[2].toInt()]!!.addFirst(stacks[l[1].toInt()]!!.remove())
+        }
+      }
+    }
+
+    return stacks.values.fold("") {top, stack -> top + stack[0] }
+  }
+
+
+  println(part1())
+}
