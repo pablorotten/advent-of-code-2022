09986cd4f7983fb55a00130e65873a9d88a447cf
PRO <pro@n-side.com>
2022-12-07 10:18:49 +0100

    day 6 part 2

--- src/main/kotlin/days/Day6.kt
+++ src/main/kotlin/days/Day6.kt
@@ -5,17 +5,19 @@ fun main() {
 
   val input = readInput("day6")
 
-  fun part1(): Int {
-    val datastream = input[0].toList()
-
+  fun detector(datastream: List<Char>, chunkSize: Int): Int {
     for (index in 1 .. datastream.size) {
-      val marker = datastream.subList(index - 1, index + 3)
-      if (marker.distinct().size == 4)
-        return index + 3
+      val marker = datastream.subList(index - 1, index + chunkSize - 1)
+      if (marker.distinct().size == chunkSize)
+        return index + chunkSize - 1
     }
 
     return -1
   }
 
+  fun part1(): Int = detector(input[0].toList(), 4)
+  fun part2(): Int = detector(input[0].toList(), 14)
+
   println(part1())
+  println(part2())
 }
