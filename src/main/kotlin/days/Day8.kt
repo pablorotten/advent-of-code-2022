d6bcd25de3f0317e8d3c7142b47a9e3895ec9b8a
PRO <pro@n-side.com>
2022-12-09 00:32:50 +0100

    day 8 part 2

--- src/main/kotlin/days/Day8.kt
+++ src/main/kotlin/days/Day8.kt
@@ -6,34 +6,34 @@ fun main() {
 
   val input = readInput("day8")
 
-  fun part1(): Int {
+  fun parseForest(
+    forestRows: List<IntArray>,
+    forestCols: List<IntArray>,
+    forest: List<String>
+  ): Pair<List<IntArray>, List<IntArray>> {
+    return if (forest.isEmpty())
+      Pair(forestRows, forestCols)
+    else
+      forest.first().let { forestLine ->
+        parseForest(
+          forestRows.plus(forestLine.map { it.digitToInt() }.toIntArray()),
+          forestLine.mapIndexed { index, tree ->
+            forestCols[index].plus(tree.digitToInt())
+          },
+          forest.drop(1)
+        )
+      }
+  }
 
-    fun parseForest(
-      forestRows: List<IntArray>,
-      forestCols: List<IntArray>,
-      forest: List<String>
-    ): Pair<List<IntArray>, List<IntArray>> {
-      return if (forest.isEmpty())
-        Pair(forestRows, forestCols)
-      else
-        forest.first().let { forestLine ->
-          parseForest(
-            forestRows.plus(forestLine.map { it.digitToInt() }.toIntArray()),
-            forestLine.mapIndexed { index, tree ->
-              forestCols[index].plus(tree.digitToInt())
-            },
-            forest.drop(1)
-          )
-        }
-    }
+  fun part1(): Int {
 
     val (rows, cols) = parseForest(List(0) { IntArray(0) }, List(input.first().length) { IntArray(0) }, input)
     var visibleTrees = 0
 
-    for (r in 1..input.size-2)
-      for (c in 1..input.first().length-2)
+    for (r in 1..input.size - 2)
+      for (c in 1..input.first().length - 2)
         rows[r][c].let { tree ->
-          if(
+          if (
             rows[r].toList().subList(0, c).none { it >= tree } ||
             rows[r].toList().subList(c + 1, rows.size).none { it >= tree } ||
             cols[c].toList().subList(0, r).none { it >= tree } ||
@@ -44,5 +44,33 @@ fun main() {
     return visibleTrees + input.size * 2 + input.first().length * 2 - 4
   }
 
+  fun part2(): Int {
+
+    val (rows, cols) = parseForest(List(0) { IntArray(0) }, List(input.first().length) { IntArray(0) }, input)
+    var bestScenicScore = 0
+
+    for (r in 1..input.size - 2)
+      for (c in 1..input.first().length - 2)
+        rows[r][c].let { tree ->
+          val scenicScore =
+            rows[r].toList().subList(0, c).reversed()
+              .let { it.takeWhile { it < tree }.plus(it.dropWhile { it < tree }.firstOrNull()) }.filterNotNull()
+              .count() *
+                rows[r].toList().subList(c + 1, cols.size)
+                  .let { it.takeWhile { it < tree }.plus(it.dropWhile { it < tree }.firstOrNull()) }.filterNotNull()
+                  .count() *
+                cols[c].toList().subList(0, r).reversed()
+                  .let { it.takeWhile { it < tree }.plus(it.dropWhile { it < tree }.firstOrNull()) }.filterNotNull()
+                  .count() *
+                cols[c].toList().subList(r + 1, rows.size)
+                  .let { it.takeWhile { it < tree }.plus(it.dropWhile { it < tree }.firstOrNull()) }.filterNotNull()
+                  .count()
+          if (scenicScore > bestScenicScore) bestScenicScore = scenicScore
+        }
+
+    return bestScenicScore
+  }
+
   println(part1())
+  println(part2())
 }
